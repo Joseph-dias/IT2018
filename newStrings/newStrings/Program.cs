5e282de71b9514b72358060c3f6e4de6df41de37
Joseph-dias <38766207+Joseph-dias@users.noreply.github.com>
2018-04-30 16:09:27 -0700

    Async Directory Info Retrieved

---
+++ newStrings/newStrings/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool done = false;
+            List<Task<List<string>>> tasks = new List<Task<List<string>>>();
+            do
+            {
+                Console.Write("Input directory path or 'exit': ");
+                string toRead = Console.ReadLine();
+                if (toRead == "exit" || toRead == "e" || toRead == "Exit") done = true;
+                else if (Directory.Exists(toRead)) tasks.Add(writeOut.getInfoAsync(toRead));
+            } while (!done);
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine($"{Environment.NewLine}Files Found{Environment.NewLine}----- -----{Environment.NewLine}{Environment.NewLine}");
+            foreach(Task<List<string>> t in tasks)
+            {
+                foreach(string s in t.Result)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
---
+++ newStrings/newStrings/writeOut.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace newStrings
+{
+    public static class writeOut
+    {
+        public static async Task<List<string>> getInfoAsync(string dName)
+        {
+            List<string> files = new List<string>();
+            DirectoryInfo d = new DirectoryInfo(dName);
+            foreach(FileInfo newFile in d.GetFiles())
+            {
+                files.Add(newFile.Name);
+            }
+            return files;
+        }
+        
+        /*private static async Task<string> getTXTAsync(string prompt)
+        {
+            Console.Write(prompt + ": ");
+            string txt = await Task.Run(() => Console.ReadLine());
+            return txt;
+        }*/
+    }
+}
